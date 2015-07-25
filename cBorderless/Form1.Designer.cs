af2f3c22dde1e37c1c90a131886591affe58c579
csm10495 <csm10495@gmail.com>
2015-07-25 12:10:40 -0700

    First commit, ability to make selected process's main window full screen border less

---
+++ cBorderless/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace cBorderless
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_make_borderless = new System.Windows.Forms.Button();
+            this.label_process = new System.Windows.Forms.Label();
+            this.combobox_processes = new System.Windows.Forms.ComboBox();
+            this.button_refresh = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // button_make_borderless
+            // 
+            this.button_make_borderless.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_make_borderless.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_make_borderless.Location = new System.Drawing.Point(76, 39);
+            this.button_make_borderless.Name = "button_make_borderless";
+            this.button_make_borderless.Size = new System.Drawing.Size(212, 63);
+            this.button_make_borderless.TabIndex = 0;
+            this.button_make_borderless.Text = "Make Selected Process Windowed Borderless";
+            this.toolTip1.SetToolTip(this.button_make_borderless, "Make the selected process windowed borderless");
+            this.button_make_borderless.UseVisualStyleBackColor = true;
+            this.button_make_borderless.Click += new System.EventHandler(this.button_make_borederless_Click);
+            // 
+            // label_process
+            // 
+            this.label_process.AutoSize = true;
+            this.label_process.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_process.Location = new System.Drawing.Point(12, 11);
+            this.label_process.Name = "label_process";
+            this.label_process.Size = new System.Drawing.Size(70, 18);
+            this.label_process.TabIndex = 1;
+            this.label_process.Text = "Process:";
+            // 
+            // combobox_processes
+            // 
+            this.combobox_processes.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.combobox_processes.FormattingEnabled = true;
+            this.combobox_processes.Location = new System.Drawing.Point(81, 9);
+            this.combobox_processes.Name = "combobox_processes";
+            this.combobox_processes.Size = new System.Drawing.Size(207, 24);
+            this.combobox_processes.Sorted = true;
+            this.combobox_processes.TabIndex = 2;
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_refresh.Font = new System.Drawing.Font("Arial", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_refresh.Location = new System.Drawing.Point(15, 39);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(55, 63);
+            this.button_refresh.TabIndex = 3;
+            this.button_refresh.Text = "↺";
+            this.toolTip1.SetToolTip(this.button_refresh, "Refresh Process List");
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.button_refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 109);
+            this.Controls.Add(this.button_refresh);
+            this.Controls.Add(this.combobox_processes);
+            this.Controls.Add(this.label_process);
+            this.Controls.Add(this.button_make_borderless);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "cBorderless - csm10495";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_make_borderless;
+        private System.Windows.Forms.Label label_process;
+        private System.Windows.Forms.ComboBox combobox_processes;
+        private System.Windows.Forms.Button button_refresh;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ cBorderless/Form1.cs
@@ -0,0 +1,119 @@
+﻿/*
+*  This file is part of cBorderless
+*  cBorderless forces applications into a full screen windowed borderless mode
+*  (C) Charles Machalow (csm10495) - MIT License
+*/
+
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace cBorderless
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Win32 Definitions
+        /// <summary>
+        /// Win32 defined constants
+        /// </summary>
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 1;
+        private const int GWL_STYLE = -16;
+        private const int GWL_EXSTYLE = -20;
+        private const long WS_SYSMENU = 0x00080000L;
+        private const long WS_POPUP = 0x80000000L;
+        private const long WS_CLIPCHILDREN = 0x02000000L;
+        private const long WS_CLIPSIBLINGS = 0x08000000L;
+        private const long WS_VISIBLE = 0x10000000L;
+        private const long WS_CAPTION = 0x00040000L;
+        private const long WS_THICKFRAME = 0x00C00000L;
+        private const long WS_MINIMIZE = 0x20000000L;
+        private const long WS_MAXIMIZE = 0x01000000L;
+        private const long WS_EX_DLGMODALFRAME = 0x00000001L;
+        private const long WS_EX_CLIENTEDGE = 0x00000200L;
+        private const long WS_EX_STATICEDGE = 0x00020000L;
+        private const uint SWP_FRAMECHANGED = 0x0020;
+        private const uint SWP_NOZORDER = 0x0004;
+        private const uint SWP_NOREPOSITION = 0x0200;
+
+        [DllImport("user32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true, EntryPoint = "SetWindowPos")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int width, int height, uint uFlags);
+
+        [DllImport("user32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true, EntryPoint = "SetWindowLong")]
+        private static extern IntPtr SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true, EntryPoint = "GetWindowLong")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        #endregion
+
+        /// <summary>
+        /// makes the process selected in the combobox fullscreen windowed borderless
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_make_borederless_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process[] SelectedProcess = System.Diagnostics.Process.GetProcessesByName(combobox_processes.SelectedItem.ToString());
+
+            if (SelectedProcess.Length > 0)
+            {
+                IntPtr hwnd = SelectedProcess[0].MainWindowHandle;
+                long style = GetWindowLong(hwnd, GWL_STYLE);
+                style &= ~(WS_CAPTION | WS_THICKFRAME | WS_MINIMIZE | WS_MAXIMIZE | WS_SYSMENU);
+                SetWindowLong(hwnd, GWL_STYLE, (int)style);
+
+                long lExStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+                lExStyle &= ~(WS_EX_DLGMODALFRAME | WS_EX_CLIENTEDGE | WS_EX_STATICEDGE);
+                SetWindowLong(hwnd, GWL_EXSTYLE, (int)lExStyle);
+                SetWindowPos(SelectedProcess[0].MainWindowHandle, IntPtr.Zero, -0, -0, Screen.GetBounds(this).Width, Screen.GetBounds(this).Height, (SWP_FRAMECHANGED | SWP_NOZORDER | SWP_NOREPOSITION));
+            }
+        }
+
+        /// <summary>
+        /// called when the refresh button is clicked
+        /// calls the refreshProcesses method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_refresh_Click(object sender, EventArgs e)
+        {
+            refreshProcesses();
+        }
+
+        /// <summary>
+        /// called on form loading
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshProcesses();
+        }
+
+        /// <summary>
+        /// Goes through all running processes and add them by name to the combobox
+        /// </summary>
+        private void refreshProcesses()
+        {
+            combobox_processes.Items.Clear();
+            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
+            foreach (System.Diagnostics.Process i in processes)
+            {
+                //don't add duplicates
+                if (!combobox_processes.Items.Contains(i.ProcessName))
+                {
+                    combobox_processes.Items.Add(i.ProcessName);
+                }
+            }
+
+            combobox_processes.Items.Remove("");
+
+            combobox_processes.SelectedIndex = 0;
+        }
+    }
+}
